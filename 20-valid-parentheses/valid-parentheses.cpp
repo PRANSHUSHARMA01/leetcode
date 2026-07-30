760b0675af4518b68540c357f7acdbec0b6e77a5
PRANSHUSHARMA01 <pranshusharma5136@gmail.com>
2026-07-30 12:27:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         stack<char> st;
+
+        for (char ch : s) {
+
+            if (ch == '{' || ch == '[' || ch == '(') {
+                st.push(ch);
+            }
+            else {
+
+                if (st.size() == 0)
+                    return false;
+
+                if (ch == '}' && st.top() == '{')
+                    st.pop();
+                else if (ch == ']' && st.top() == '[')
+                    st.pop();
+                else if (ch == ')' && st.top() == '(')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+
+        if (st.size() == 0)
+    return true;
+else
+    return false;
+    }};
