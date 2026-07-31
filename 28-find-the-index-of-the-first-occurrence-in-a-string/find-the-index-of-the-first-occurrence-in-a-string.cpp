35beebffc6a330b35b81216a1bf59ddb242bfc8c
PRANSHUSHARMA01 <pranshusharma5136@gmail.com>
2026-07-31 21:59:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.15%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+            if (needle.size() > haystack.size()){
+            return -1;}
+
+        for (int i = 0; i + needle.size() <= haystack.size(); i++) {
+
+            int j;
+            for (j = 0; j < needle.size(); j++) {
+                if (haystack[i + j] != needle[j])
+                    break;
+            }
+
+            if (j == needle.size())
+                return i;
+        }
+
+        return -1;}
+};
